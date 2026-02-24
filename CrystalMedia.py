a6ff68ceac95fc4c7a66b9a0d51e35a82aff3057
Thegamerprogrammer <91685655+Thegamerprogrammer@users.noreply.github.com>
2026-02-24 22:15:42 +0530

    Update CrystalMedia.py

--- CrystalMedia.py
+++ CrystalMedia.py
@@ -61,7 +61,7 @@ def create_folders():
 create_folders()
 
 # ──────────────────────────────────────────────
-# YouTube ripping logic
+# YouTube logic
 # ──────────────────────────────────────────────
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
@@ -251,4 +251,5 @@ def main_loop():
             time.sleep(2)
 
 if __name__ == "__main__":
+
     main_loop()
